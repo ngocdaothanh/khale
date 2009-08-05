43a2a09c5b25c59dbe0aa19b68e335a8a333eda2
Ngoc Dao <ngocdaothanh@gmail.com>
2009-08-06 04:03:06 +0900

    Use Google chart for poll

--- removable/article/v_article_show.erl
+++ removable/article/v_article_show.erl
@@ -8,11 +8,12 @@ render() ->
     ale:app(title_in_body, h_article:render_name()),
 
     Article = ale:app(article),
-    ale:app(title_in_head, h_article:render_title(Article)),
+    TitleInHead = h_article:render_title(Article),
+    ale:app(title_in_head, TitleInHead),
 
     User = m_user:find(Article#article.user_id),
     [
-        {h1, [], h_article:render_title(Article)},
+        {h1, [], TitleInHead},
         h_user:render(User),
         {'div', [], [Article#article.abstract, Article#article.body]},
         h_discussion:render_all(article, Article#article.id)
--- removable/qa/v_qa_show.erl
+++ removable/qa/v_qa_show.erl
@@ -8,11 +8,12 @@ render() ->
     ale:app(title_in_body, h_qa:render_name()),
 
     Qa = ale:app(qa),
-    ale:app(title_in_head, h_qa:render_title(Qa)),
+    TitleInHead = h_qa:render_title(Qa),
+    ale:app(title_in_head, TitleInHead),
 
     User = m_user:find(Qa#qa.user_id),
     [
-        {h1, [], h_qa:render_title(Qa)},
+        {h1, [], TitleInHead},
         h_user:render(User),
         {'div', [], Qa#qa.detail},
         h_discussion:render_all(qa, Qa#qa.id)
