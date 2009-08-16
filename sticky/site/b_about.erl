367afd098310082e6567b170bef3d4ab9da654a1
Linh Tran <linhtran@toran-rins-macbook.local>
2009-08-17 01:06:07 +0900

    Add icons

--- sticky/site/b_about.erl
+++ sticky/site/b_about.erl
@@ -9,20 +9,29 @@ render(_Id, _Data) ->
 
     % Link to users is hidden if there is no user
     UsersLink = case mnesia:table_info(user, size) of
-        0        -> [];
-        NumUsers -> {li, [], {a, [{href, ale:path(user,  index)}], ?TF("~p users", [NumUsers])}}
+        0 -> [];
+
+        NumUsers ->
+            {li, [], [
+                {img, [{src, "/static/img/user.png"}]}, " ",
+                {a, [{href, ale:path(user,  index)}], ?TF("~p users", [NumUsers])}
+            ]}
     end,
 
-    FeedLink = {li, [],
+    FeedLink = {li, [], [
+        {img, [{src, "/static/img/feed.png"}]}, " ",
         {a, [{href, ale:path(content, feed)}], ?TF("~p contents", [mnesia:table_info(thread, size)])}
-    },
+    ]},
 
     % Link to discussions is always displayed, so that the first one can be created
     DiscussionsLinkText = case m_discussion:count(site, undefined) of
         0              -> ?T("Discuss about this site");
         NumDiscussions -> ?TF("~p discussions about this site", [NumDiscussions])
     end,
-    DiscussionsLink = {li, [], {a, [{href, ale:path(site, about)}], DiscussionsLinkText}},
+    DiscussionsLink = {li, [], [
+        {img, [{src, "/static/img/about.png"}]}, " ",
+        {a, [{href, ale:path(site, about)}], DiscussionsLinkText}
+    ]},
 
     Body = [
         Site#site.about,
